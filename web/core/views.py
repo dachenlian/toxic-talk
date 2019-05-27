fe58c2fbfffb517813ede07c13a5227d432efdf8
richl <cclemon.lian@gmail.com>
2019-05-27 12:36:25 +0800

    added form

--- web/core/views.py
+++ web/core/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+import requests
+
 
 class IndexView(View):
+    template_name = 'core/index.html'
+
     def get(self, request, *args, **kwargs):
-        return render(request, 'core/index.html')
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        text = request.POST.get('seed')
+        data = {
+            'seed': text,
+        }
+        r = request.post(
+            'url',
+            data=data,
+        )
+        context = {
+            'result': r,
+        }
+        return render(request, self.template_name, context=context)
